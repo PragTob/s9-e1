82cd2035737487ab14a66ee986cb28b45aff6095
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2011-09-12 15:38:35 +0200

    dual purpose accessors refactoring to use aliasing for the = methods

--- lib/odfdom/open_style.rb
+++ lib/odfdom/open_style.rb
@@ -12,13 +12,9 @@ class OpenStyle
    # instance_eval(&block) if block_given?
   end
 
-  def display_name=(name)
-    @style.style_display_name_attribute = name
-  end
-
   def display_name(name=nil)
     if name
-      self.display_name = name
+      @style.style_display_name_attribute = name
     else
       @style.style_display_name_attribute
     end
@@ -28,47 +24,45 @@ class OpenStyle
   # idea taken from the odfdom tutorial
   # http://www.langintro.com/odfdom_tutorials/create_odt.html
 
-  def font_weight=(value)
-    @style.set_property(StyleTextPropertiesElement::FontWeight, value);
-		@style.set_property(StyleTextPropertiesElement::FontWeightAsian, value);
-		@style.set_property(StyleTextPropertiesElement::FontWeightComplex, value);
-  end
-
   def font_weight(value=nil)
     if value
-      self.font_weight = value
+      @style.set_property(StyleTextPropertiesElement::FontWeight, value)
+		  @style.set_property(StyleTextPropertiesElement::FontWeightAsian, value)
+		  @style.set_property(StyleTextPropertiesElement::FontWeightComplex, value)
     else
       @style.get_property(StyleTextPropertiesElement::FontWeight)
     end
   end
 
-  def font_style=(value)
-    @style.set_property(StyleTextPropertiesElement::FontStyle, value);
-		@style.set_property(StyleTextPropertiesElement::FontStyleAsian, value);
-		@style.set_property(StyleTextPropertiesElement::FontStyleComplex, value);
-  end
-
   def font_style(value=nil)
     if value
-      self.font_style = value
+      @style.set_property(StyleTextPropertiesElement::FontStyle, value)
+		  @style.set_property(StyleTextPropertiesElement::FontStyleAsian, value)
+		  @style.set_property(StyleTextPropertiesElement::FontStyleComplex, value)
     else
       @style.get_property(StyleTextPropertiesElement::FontStyle)
     end
   end
 
   def font_size=(value)
-    @style.set_property(StyleTextPropertiesElement::FontSize, value);
-		@style.set_property(StyleTextPropertiesElement::FontSizeAsian, value);
-		@style.set_property(StyleTextPropertiesElement::FontSizeComplex, value);
+
   end
 
   def font_size(value=nil)
     if value
-      self.font_size = value
+      @style.set_property(StyleTextPropertiesElement::FontSize, value)
+	  	@style.set_property(StyleTextPropertiesElement::FontSizeAsian, value)
+		  @style.set_property(StyleTextPropertiesElement::FontSizeComplex, value)
     else
       @style.get_property(StyleTextPropertiesElement::FontSize)
     end
   end
+
+  # aliasing to also get the other cool setters
+  alias_method :display_name=, :display_name
+  alias_method :font_weight=, :font_weight
+  alias_method :font_size=, :font_size
+  alias_method :font_style=, :font_style
 
 end
 
