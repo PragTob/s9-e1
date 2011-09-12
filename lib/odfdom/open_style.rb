aab1fa6493cb7820f08cd2e4569267f211d823b3
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2011-09-12 02:15:10 +0200

    First little open_style implementation

---
+++ lib/odfdom/open_style.rb
@@ -0,0 +1,36 @@
+require 'java'
+require '../bin/odfdom-java-0.8.7-jar-with-dependencies.jar'
+java_import org.odftoolkit.odfdom.incubator.doc.style.OdfStyle
+java_import org.odftoolkit.odfdom.doc.style.OdfStyleTextProperties
+
+class OpenStyle
+
+  # initialize given our style
+  def initialize style
+    @style = style
+  end
+
+  # REMARK: the next 3 methods exist for internationalization reasons
+  # idea taken from the odfdom tutorial
+  # http://www.langintro.com/odfdom_tutorials/create_odt.html
+
+  def font_weight(value)
+    @style.property(OdfStyleTextProperties.FontWeight, value);
+		@style.property(OdfStyleTextProperties.FontWeightAsian, value);
+		@style.property(OdfStyleTextProperties.FontWeightComplex, value);
+  end
+
+  def font_style(value)
+    @style.property(OdfStyleTextProperties.FontStyle, value);
+		@style.property(OdfStyleTextProperties.FontStyleAsian, value);
+		@style.property(OdfStyleTextProperties.FontStyleComplex, value);
+  end
+
+  def font_size(value)
+    @style.property(OdfStyleTextProperties.FontSize, value);
+		@style.property(OdfStyleTextProperties.FontSize, value);
+		@style.property(OdfStyleTextProperties.FontSize, value);
+  end
+
+end
+
