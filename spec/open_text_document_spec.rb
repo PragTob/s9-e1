9cd69c27168eace956626ea75ae0dc4f606be5ef
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2011-09-13 14:19:39 +0200

    Moved the style specific test to an own spec

--- spec/open_text_document_spec.rb
+++ spec/open_text_document_spec.rb
@@ -94,14 +94,5 @@ describe "OpenTextDocument" do
 
   end
 
-  # added toprotect against regressions
-  it "should be able to create styles in a block" do
-    style = @doc.new_style("test", :paragraph) do
-      display_name "A good test"
-    end
-
-    style.display_name.should == "A good test"
-  end
-
 end
 
